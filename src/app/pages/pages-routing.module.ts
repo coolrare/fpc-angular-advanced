ae8b1a8d6b704edf7f12af502590316282bf41bb
Mike Huang <wellwind@mail2000.com.tw>
2020-10-19 10:23:06 +0800

    練習建立模組下的元件及路由設定

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,12 +1,22 @@
+import { BlankComponent } from './blank/blank.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PagesComponent } from './pages.component';
 
-const routes: Routes = [{ path: '', component: PagesComponent }];
+const routes: Routes = [
+  // 不透過 PagesComponent 直接顯示元件
+  // { path: 'blank', component: BlankComponent },
+  // 透過 PagesComponent 的 router-outlet 顯示子路由
+  {
+    path: '',
+    component: PagesComponent,
+    children: [{ path: 'blank', component: BlankComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
-
+import { BlankComponent } from './blank/blank.component';
 
 @NgModule({
-  declarations: [PagesComponent],
+  declarations: [PagesComponent, BlankComponent],
   imports: [
     CommonModule,
     PagesRoutingModule
