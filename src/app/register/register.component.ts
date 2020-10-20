17d754ff84a0f5429f8e8f986a1d4dddd7c993fc
Mike Huang <wellwind@mail2000.com.tw>
2020-10-20 13:42:40 +0800

    練習使用巢狀 FormGroup

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   form = this.formBuilder.group({
-    firstName: 'Mike'
+    firstName: 'Mike',
+    lastName: 'Huang',
+    passwordGroup: this.formBuilder.group({
+      password: '',
+      repeatPassword: '',
+    }),
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  // form2 = this.formBuilder.group({
+  //   firstName: this.formBuilder.control('Mike')
+  // });
 
-  ngOnInit(): void {
+  // form3 = new FormGroup({
+  //   firstName: new FormControl()
+  // });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  register() {
+    console.log(this.form.value);
   }
-
 }
